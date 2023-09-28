b79e72184f204b5ec5eeacd9be5bef3c069c903e
saikumarponnaganti <97146447+saikumarponnaganti99@users.noreply.github.com>
2023-09-28 14:00:51 +0530

    first commit

---
+++ fire.js
@@ -0,0 +1,50 @@
+
+let bg_backgroundEl = document.getElementById("bg_background");
+
+let X=document.getElementById("popup1");
+function openPopup1(){
+    X.classList.add("open-popup");
+    bg_backgroundEl.classList.add("changeColorBackground");
+}        
+function closePopup1(){
+    X.classList.remove("open-popup");
+    bg_backgroundEl.classList.remove("changeColorBackground");
+}
+
+
+let A=document.getElementById("popup2");
+function openPopup2(){
+    A.classList.add("open-popup");
+    bg_backgroundEl.classList.add("changeColorBackground");
+}
+function closePopup2(){
+    A.classList.remove("open-popup");
+    bg_backgroundEl.classList.remove("changeColorBackground");
+}
+let B=document.getElementById("popup3");
+function openPopup3(){
+    B.classList.add("open-popup");
+    bg_backgroundEl.classList.add("changeColorBackground");
+}
+function closePopup3(){
+    B.classList.remove("open-popup");
+    bg_backgroundEl.classList.remove("changeColorBackground");
+}
+let C=document.getElementById("popup4");
+function openPopup4(){
+    C.classList.add("open-popup");
+    bg_backgroundEl.classList.add("changeColorBackground");
+}
+function closePopup4(){
+    C.classList.remove("open-popup");
+    bg_backgroundEl.classList.remove("changeColorBackground");
+}
+let D=document.getElementById("popup5");
+function openPopup5(){
+    D.classList.add("open-popup");
+    bg_backgroundEl.classList.add("changeColorBackground");
+}
+function closePopup5(){
+    D.classList.remove("open-popup")
+    bg_backgroundEl.classList.remove("changeColorBackground");
+}
